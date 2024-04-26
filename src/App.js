417a6ca305dc0f63f8d1cc51867fc6ee3647d5dc
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-04-26 14:37:11 +0530

    new file:   src/App.css 	new file:   src/App.js 	modified:   src/index.js

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+import './App.css';
+
+const App = () => {
+    const [result,setResult] = useState("");
+
+    const handleClick = (e) => {
+            setResult(result.concate(e.target.name));
+    }
+  return (
+    <div className='container'>
+        <form>
+            <input type='text' value={result}/>
+        </form>
+        <div className='keypad'>
+            <button onClick={handleClick}>Clear</button>
+            <button onClick={handleClick}>C</button>
+            <button onClick={handleClick}>&divide;</button>
+            <button onClick={handleClick}>7</button>
+            <button onClick={handleClick}>8</button>
+            <button onClick={handleClick}>9</button>
+            <button onClick={handleClick}>&times;</button>
+            <button onClick={handleClick}>4</button>
+            <button onClick={handleClick}>5</button>
+            <button onClick={handleClick}>6</button>
+            <button onClick={handleClick}>&ndash;</button>
+            <button onClick={handleClick}>1</button>
+            <button onClick={handleClick}>2</button>
+            <button onClick={handleClick}>3</button>
+            <button onClick={handleClick}>+</button>
+            <button onClick={handleClick}>0</button>
+            <button onClick={handleClick}>.</button>
+            <button onClick={handleClick}>=</button>
+            
+            
+        </div>
+    </div>
+  );
+}
+
+export default App
